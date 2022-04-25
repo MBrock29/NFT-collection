67b5def775b80f22b966368a304cca93cbec52ff
MBrock29 <55901037+MBrock29@users.noreply.github.com>
2022-04-25 14:56:33 +0100

    remove folder

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x178f5400e92b600a4F46399713f4D648B4D80cE7";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
